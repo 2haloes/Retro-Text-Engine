3d48411543922b2ae5d15d19febfb4a48e6cb5eb
Rhys Henson <rhysh@live.co.uk>
2019-07-12 11:19:24 +0100

    Text displays properly, sound does not play properly

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 $(function() {
-    textToDisplay = ["Hello there", "*Gasp!*"]
+    // Array to store values, edit this to edit the output
+    textToDisplay = [
+        "Hello there",
+        "*Gasp!*"
+    ];
+
+    textAudio = new Audio("talking.mp3");
+    textIndex = 0;
 
     document.getElementById("textBox").addEventListener("click", function() {
         nextDisplay();
     });
+
+    nextDisplay();
 });
 
 let textToDisplay;
+let textToDisplayChar;
 let textIndex;
+let charIndex;
+let textOutputInterval;
+let textAudio;
 
 function nextDisplay() {
     // [...Foo] creates an array from the Foo variable, this is used here to split the string into chars
     // This is used as it allows support for special characters (unlike a function like Foo.split('') which would output broken characters in that case)
-    let textToDisplayChar = [...textToDisplay[textIndex]];
-
+    textToDisplayChar = [...textToDisplay[textIndex]];
+    textAudio.loop = true;
+    charIndex = 0;
+    textAudio.play();
+    $('#textOutput').text('');
+    textOutputInterval = setInterval(textOutput, 200);
+    textAudio.loop = false;
     textIndex++;
 }
 
+function textOutput() {
+    if (charIndex < textToDisplayChar.length) {
+        $('#textOutput').text($('#textOutput').text() + textToDisplayChar[charIndex]);
+        charIndex++;
+    } else {
+        clearInterval(textOutputInterval);
+        return;
+    }
+}
